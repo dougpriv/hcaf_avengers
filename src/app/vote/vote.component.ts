aae1730304f8fea8b298ea5a5742fbf771ed6f38
Douglas Lorenzen <douglas.lorenzen@cdillc.com>
2021-02-23 11:13:04 -0500

    HA-3: Break-fix

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -62,18 +62,14 @@ export class VoteComponent {
 
   castVote() {
     if (this.selectedHero == null) this.votedText = "Hey! Pick a Hero!"
-else {
+
      localStorage.setItem('voted', 'true');
     this.http.post('http://' + environment.backend + '/castVote', { hero: this.selectedHero })
     .subscribe(res => {
       this.getVotes();
       this.getVoteText();
     }, (err) => { console.log('err: ' + JSON.stringify(err)); })  
-}
 
-    
-
-    
   }
 
   getVoteText() {
